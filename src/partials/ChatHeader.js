ba9f3faf1d62ef96ec4330133b396d6cbca3388c
mhxx307 <minhquan.lavo@gmail.com>
2024-01-30 13:55:26 +0700

    Setup darkmode/lightmode

--- src/partials/ChatHeader.js
+++ src/partials/ChatHeader.js
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
 import { RiLogoutBoxRLine } from "react-icons/ri";
-import { HiSun } from "react-icons/hi2";
+import { HiSun, HiMoon } from "react-icons/hi";
+import { useTheme } from "../hooks/useTheme";
 
 function ChatHeader() {
+    const { isDarkMode, toggleDarkMode } = useTheme();
+
     return (
-        <header className="bg-gray-700 text-white p-4">
+        <header className={`${isDarkMode ? "text-white bg-black" : "text-black bg-white"} p-4`}>
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-semibold">
-                    <Link to="/" className="text-white">
-                        Website Name
-                    </Link>
+                    <Link to="/">Website Name</Link>
                 </h1>
                 <nav>
-                    <ul className="flex space-x-4">
-                        <li className="cursor-pointer">
-                            <HiSun className="inline-block" size={24} />
+                    <ul className="flex space-x-4 items-center">
+                        <li className="cursor-pointer" onClick={toggleDarkMode}>
+                            {isDarkMode ? (
+                                <HiSun className="inline-block" size={24} />
+                            ) : (
+                                <HiMoon className="inline-block" size={24} />
+                            )}
                         </li>
 
                         <li className="cursor-pointer">
